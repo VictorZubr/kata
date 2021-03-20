b4deb25aa3ca60a477ca3fe3d0bdc4858c8a873e
zubr <zubr@rambler.ru>
2021-03-20 20:35:51 +0300

    distance between points

---
+++ 08/distance-between-points/distance-between-points.js
@@ -0,0 +1,3 @@
+const distanceBetweenPoints = ({x: x1, y: y1}, {x: x2, y: y2}) => Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+
+// const distanceBetweenPoints = ({ x: x1, y: y1 }, { x: x2, y: y2 }) => Math.hypot(x1 - x2, y1 - y2)
