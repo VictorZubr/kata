d34b595f6b97074f000d2b75caf4b1f6af6d01df
zubr <zubr@rambler.ru>
2021-03-21 12:54:37 +0300

    ball

---
+++ 08/ball-class/ball.js
@@ -0,0 +1,12 @@
+class Ball {
+    constructor(str) {
+        this.ballType = str ? str : 'regular';
+    }
+}
+
+/*
+class Ball {
+    constructor(ballType = "regular") {
+        this.ballType = ballType;
+    }
+}*/
